e0471576d7fe6eb898f861e8bc354a828ebbf72c
sandeep7052 <63717826+sandeep7052@users.noreply.github.com>
2023-10-28 19:56:27 +0530

    Create test1.java

---
+++ test1.java
@@ -0,0 +1,17 @@
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello welcome sandeep");
+		int i=10;
+		if(i<11) {
+			System.out.println("True");
+		}
+		else {
+			System.out.print(false);
+		}
+		
+
+	}
+
+}
